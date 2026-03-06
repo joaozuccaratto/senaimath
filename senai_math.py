cd8a613f6d9acdf96bd570711326b9abaffb284a
GUILHERME SOARES CARDOSO <47400958828@info.local>
2026-03-05 22:09:52 -0300

    Inserção da função euler

--- senai_math.py
+++ senai_math.py
@@ -17,3 +17,17 @@ radianos = graus * pi / 180
 resultado = seno_taylor(radianos, 15)
 
 print("Seno de 90 graus ≈", resultado)
+
+def calcular_fatorial(n):
+     contador=n
+     if n <0:
+          print("erro: Invalido")
+          return None
+     if n == 0:
+          n=1
+          return n
+     if n > 0:
+         for i in range(1,n):
+              contador-=1
+              n = n*contador
+         return n
